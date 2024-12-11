93b35e5fbc04224e2de229403794cc7ea5737895
Max Sokolski <cmltawt0@gmail.com>
2024-12-11 23:11:19 +0200

    fix: override CACHES django setting value

--- tutorrgg/templates/rgg/apps/settings/partials/common.py
+++ tutorrgg/templates/rgg/apps/settings/partials/common.py
@@ -25,12 +25,22 @@ DATABASES = {
     }
 }
 
+CACHES = {
+    "default": {
+        "BACKEND": "redis_cache.RedisCache",
+        "LOCATION": "redis:6379",
+        "OPTIONS": {
+            'DB': 3,
+        },
+    }
+}
+
 MONGO_URL = "mongodb://mongodb:27017/"
 MONGO_DATABASE = "gamma_data"
 
 EDX_LMS_BASE_URL = "http://lms:8000"
 EDX_API_KEY = "{{ EDX_API_KEY }}"
 
-CELERY_BROKER_URL = "redis://redis:6379/2"
+CELERY_BROKER_URL = "redis://redis:6379/3"
 
 {{ patch("rgg-common-settings") }}
