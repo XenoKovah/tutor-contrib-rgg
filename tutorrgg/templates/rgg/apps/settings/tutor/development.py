fecafe0144a3b028d9283090a2e9a68925bd7913
Mykhailo Chernyshov <mykhailo.chernyshov@raccoongang.com>
2025-07-24 11:16:22 +0000

    fix: [TEA-205] Media settings are fixed for dev environment

--- tutorrgg/templates/rgg/apps/settings/tutor/development.py
+++ tutorrgg/templates/rgg/apps/settings/tutor/development.py
@@ -1,10 +1,10 @@
 from ..devstack import *
 
+{% include "rgg/apps/settings/partials/common.py" %}
+
 {% if IS_MINIO_ENABLED %}
 AWS_S3_ENDPOINT_URL = "http://{{ MINIO_HOST }}:9000"
 {% endif %}
-
-{% include "rgg/apps/settings/partials/common.py" %}
 
 EDX_LMS_BASE_URL = "http://{{ LMS_HOST }}:8000"
 
